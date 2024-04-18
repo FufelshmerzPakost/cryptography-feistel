4cb6d0232f7741871e02f48171ec0275f82bf07d
FufelshmerzPacost <stas.boyko004@gmail.com>
2024-04-18 11:26:25 +0000

    t

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+
+
+
+fn gen_keys
+
+fn block_crypt(value: u32){
+
+}
+
+fn f(part:i32, key: i32) -> u8 {
+    0
+}
+
+// 64 bit
+fn block_encrypt() -> u16 {
+
+}
+
+fn main() {
+    let text = b"boykostanislav".bin();
+    println!("{:?}", text.as_bytes());
+}
